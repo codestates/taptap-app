899001ba674eb227ad147aef9c77f54d7d8dee40
SeongJun Park <tjdwns4850@gmail.com>
2019-04-04 21:38:59 +0900

    쿠폰 아이템 이미지 변경 & css

--- components/Coupons/CouponsDisplay.js
+++ components/Coupons/CouponsDisplay.js
@@ -1,40 +1,48 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
+const styles = StyleSheet.create({
+  couponsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: '#F2F2F2',
+    justifyContent: 'center',
+    height: 180,
+    width: '90%',
+    paddingTop: 20
+  }
+});
 export default class CouponsDisplay extends Component {
   render() {
     const { count, required } = this.props;
     const coupons = [];
     for (let i = 0; i < count; i++) {
       coupons.push(
-        <Image
-          key={`checked${i}`}
-          style={{ width: 50, height: 50 }}
-          source={{
-            uri: 'https://img.icons8.com/ios/100/000000/warranty-filled.png'
-          }}
-        />
+        // <Image
+        //   key={`checked${i}`}
+        //   style={{ width: 70, height: 70 }}
+        //   source={{
+        //     uri: 'https://img.icons8.com/ios/100/000000/warranty-filled.png'
+        //   }}
+        // />
+        <AntDesign key={`checked${i}`} name={'star'} size={70} />
       );
     }
     for (let i = 0; i < required - count; i++) {
       coupons.push(
-        <Image
-          key={`unchecked${i}`}
-          style={{ width: 50, height: 50 }}
-          source={{
-            uri: 'https://img.icons8.com/ios/100/000000/warranty.png'
-          }}
-        />
+        // <Image
+        //   key={`unchecked${i}`}
+        //   style={{ width: 70, height: 70 }}
+        //   source={{
+        //     uri: 'https://img.icons8.com/ios/100/000000/warranty.png'
+        //   }}
+        // />
+        <AntDesign key={`unchecked${i}`} name={'staro'} size={70} />
       );
     }
     return (
-      <View
-        style={{
-          flexDirection: 'row'
-        }}
-      >
-        {coupons.map(img => img)}
-      </View>
+      <View style={styles.couponsContainer}>{coupons.map(img => img)}</View>
     );
   }
 }
