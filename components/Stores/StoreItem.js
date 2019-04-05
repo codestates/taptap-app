b92a95c7a0cfd9aa7a46a8c72dc8bdd34f0efe87
SeongJun Park <tjdwns4850@gmail.com>
2019-04-05 16:20:51 +0900

    쿠폰 있는 매장들 CSS

--- components/Stores/StoreItem.js
+++ components/Stores/StoreItem.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { TouchableHighlight, Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    backgroundColor: 'red',
-    width: 200
+  storeItemContainer: {
+    width: '100%',
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  button: {
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 10
   }
 });
 export default class StoreItem extends Component {
   render() {
     const { id, name, onPress } = this.props;
     return (
-      <Text style={styles.container} onPress={() => onPress(id)}>
-        {' '}
-        {name}{' '}
-      </Text>
+      <View style={styles.storeItemContainer}>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => onPress(id)}
+          underlayColor={'#FDC20E'}
+        >
+          <Text
+            style={{
+              fontSize: 30
+            }}
+          >
+            {name}
+          </Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
--- components/Stores/index.js
+++ components/Stores/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import StoreItem from './StoreItem';
 
+const styles = StyleSheet.create({
+  storeView: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F2F2F2',
+    marginTop: 20,
+    marginRight: 20,
+    marginBottom: 20,
+    marginLeft: 20
+  }
+});
 export default class Stores extends Component {
   render() {
     const { visitedStores, onPress } = this.props;
     return (
-      <View>
+      <View style={styles.storeView}>
         {visitedStores.map((store, idx) => {
           return (
             <StoreItem
